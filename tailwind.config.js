475f929935c4577b5fa693a14e41ecf16ac2d36d
Charel Felten <charel.felten@gmail.com>
2024-10-13 18:51:20 +0200

    feverdream to get tailwind to work again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{vue,js,ts,jsx,tsx}",
-    ],
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
-        colors: {
-            'zg1': '#78dca0',
-            'zg2': '#032323',
-        }
+        extend: {
+            colors: {
+                'zg1': '#78dca0',
+                'zg2': '#032323',
+            }
+        },
     },
     plugins: [],
 }
