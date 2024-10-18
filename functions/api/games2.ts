6807fb822555c869dc447a8fa2cb6d8cfc64a87b
Charel Felten <charel.felten@gmail.com>
2024-10-18 15:13:03 +0200

    lfg

--- functions/api/games2.ts
+++ functions/api/games2.ts
@@ -2,6 +2,12 @@ interface Env {
     DB: D1Database;
 }
 
+type Games2PostBody = {
+    winner: number;
+    loser: number;
+    dt: string;
+};
+
 export const onRequestGet: PagesFunction<Env> = async (context) => {
     const { searchParams } = new URL(context.request.url);
     const amount = parseInt(searchParams.get('limit') ?? '20', 10);
@@ -26,5 +32,20 @@ export const onRequestGet: PagesFunction<Env> = async (context) => {
     `).bind(amount);
     const { results } = await ps.all();
     return Response.json(results);
+};
+
+export const onRequestPost: PagesFunction<Env> = async (context) => {
+    try {
+        const body: Games2PostBody = await context.request.json();
+        const { winner, loser, dt } = body;
+        if (winner === loser) {
+            return new Response("Winner can't be the same as loser", { status: 400 });
+        }
+        const ps = context.env.DB.prepare("INSERT INTO games (winner, loser, dt) VALUES (?1, ?2, ?3)").bind(winner, loser, dt);
+        const { success } = await ps.run();
+        return Response.json(success)
+    } catch (error) {
+        return new Response(JSON.stringify({ error: error.message }), { status: 400 });
+    }
 };
 
